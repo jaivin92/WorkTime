c05e33b62050476f9ce270cf41ae9a500b9cffaa
USER <kishan9.3mungara@gmail.com>
2021-01-24 11:24:24 +0530

    ok9pilkil
    Signed-off-by: USER <kishan9.3mungara@gmail.com>

---
+++ JayWin/src/main/java/example/com/jaywin/ViewPager.java
@@ -0,0 +1,16 @@
+package example.com.jaywin;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class ViewPager {
+
+    public static void s(Context c, String message){
+
+        Toast.makeText(c,message, Toast.LENGTH_SHORT).show();
+
+    }
+
+}
+
+
